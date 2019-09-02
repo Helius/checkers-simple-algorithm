1e78c15a67e980205f82548048926fdfd99da903
Eugene Samoylov <e.samoylov@2gis.ru>
2019-09-02 11:29:33 +0700

    work in progress

--- checkers.cpp
+++ checkers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <assert.h>
 
 
 enum Field{
@@ -8,28 +9,96 @@ enum Field{
 	Black,
 };
 
+class Step {
+public:
+	Step(){};
+	Step(int f, int t)
+		: from(f)
+		, to(t)
+	{}
+public:
+	bool isValid() const
+	{
+		return (from != -1) && (to != -1) && (from!= to);
+	}
+	int from = -1;
+	int to = -1;
+};
+
+class Move {
+public:
+	Move(Step step) {
+		steps_[0] = step;
+	}
+	bool addStep(Step step)
+	{
+		if(stepCount_ == maxSteps_)
+		{
+			return false;
+		}
+		steps_[stepCount_] = step;
+		stepCount_++;
+		return true;
+	}
+	const Step getStep(int ind) const
+	{
+		if(ind >= maxSteps_)
+		{
+			return Step();
+		}
+		return steps_[ind];
+	}
+private:
+	static constexpr int maxSteps_ = 6;
+	int stepCount_ = 0;
+	Step steps_[maxSteps_];
+};
+
 // all checkers set on black
 // 1A is black
 class Board {
 
 public:	
 
-	Board(bool imWhite = false)
-		: imWhite_(imWhite)
+	Board()
 	{
+		reset();
+	}
+
+	void reset() {
 		for(int i = 0; i < 12; ++i)
 		{
-			fields[i] = imWhite ? White : Black;
-  		fields[size_-i-1] = imWhite ? Black : White;
+			fields[i] = White;
+  		fields[size_-i-1] = Black;
+		}
+		for(int i = 12; i < 20; ++i)
+		{
+			fields[i] = Empty;
 		}
 	}
 
 	int size() const { return size_; }
+	
 	Field get(int i) const { return fields[i]; }
+
+	void doMove(const Move & move)
+	{
+		int ind = 0;
+		while(move.getStep(ind).isValid())
+		{
+			Step step = move.getStep(ind);
+			fields[step.to] = fields[step.from];
+			fields[step.from] = Empty;
+			ind++;
+		}
+	}
+	void remove(int i)
+	{
+	
+	}
 
 private:
 	static constexpr int size_ = 32;
-	bool imWhite_ = false;
 	Field fields[size_] = {Empty};
 };
 
@@ -86,10 +155,10 @@ void printBoard(const Board & b)
 	std::cout << "\u2513";
 	std::cout << std::endl;
 
-	for(int i = 0; i < 8; ++i)
+	for(int i = 8; i > 0; --i)
 	{
-		std::cout << (char)('h'-i) << " ";
-		printLine(i, b);	
+		std::cout << (char)('h'- i + 1) << " ";
+		printLine(i-1, b);	
 		std::cout << "  ";
 		std::cout << "\u2523";
 		if(i < 7) {
@@ -119,11 +188,114 @@ void printBoard(const Board & b)
 	std::cout << std::endl;
 }
 
+class CheckersAI {
+
+public:	
+	bool checkMove(const Board & b, const Move & m) 
+	{
+		return true;
+	}
+
+	void applyMove(const Board & b, const Move & m)
+	{
+	}
+
+	Move findMyMove(const Board & b)
+	{
+		return Move(Step(1,2));
+	}
+
+};
+
+class Game {
+public:
+	void reset() 
+	{}
+
+	bool applyTheirMove()
+	{
+		return true;
+	}
+
+	bool doMyMove()
+	{
+		return true;
+	}
+
+private:
+	bool myMove_ = false;
+	Board b;
+};
+
 int main()
 {
+/*
+  ======= Test zone =======
+*/
+	{
+		Board b;
+		assert((b.get(0) == White) && "Board init error: first isn't white");
+		assert((b.get(1) == White) && "Board init error: isn't white");
+		assert((b.get(2) == White) && "Board init error: isn't white");
+		assert((b.get(3) == White) && "Board init error: isn't white");
+		assert((b.get(4) == White) && "Board init error: isn't white");
+		assert((b.get(5) == White) && "Board init error: isn't white");
+		assert((b.get(6) == White) && "Board init error: isn't white");
+		assert((b.get(7) == White) && "Board init error: isn't white");
+		assert((b.get(8) == White) && "Board init error: isn't white");
+		assert((b.get(9) == White) && "Board init error: isn't white");
+		assert((b.get(10) == White) && "Board init error: isn't white");
+		assert((b.get(11) == White) && "Board init error: isn't white");
+		
+		assert((b.get(12) == Empty) && "Board init error: empty");
+		assert((b.get(13) == Empty) && "Board init error: empty");
+		assert((b.get(14) == Empty) && "Board init error: empty");
+		assert((b.get(15) == Empty) && "Board init error: empty");
+		assert((b.get(16) == Empty) && "Board init error: empty");
+		assert((b.get(17) == Empty) && "Board init error: empty");
+		assert((b.get(18) == Empty) && "Board init error: empty");
+		assert((b.get(19) == Empty) && "Board init error: empty");
+
+		assert((b.get(20) == Black) && "Board init error: isn't black");
+		assert((b.get(21) == Black) && "Board init error: isn't black");
+		assert((b.get(22) == Black) && "Board init error: isn't black");
+		assert((b.get(23) == Black) && "Board init error: isn't black");
+		assert((b.get(24) == Black) && "Board init error: isn't black");
+		assert((b.get(25) == Black) && "Board init error: isn't black");
+		assert((b.get(26) == Black) && "Board init error: isn't black");
+		assert((b.get(27) == Black) && "Board init error: isn't black");
+		assert((b.get(28) == Black) && "Board init error: isn't black");
+		assert((b.get(29) == Black) && "Board init error: isn't black");
+		assert((b.get(30) == Black) && "Board init error: isn't black");
+		assert((b.get(31) == Black) && "Board init error: isn't black");
+	}
+	{
+		Board b;
+		Step step(8,12);
+		Move move(step);
+		b.doMove(move);
+		assert(b.get(8) == Empty && b.get(12) == White && "Check board move white");
+
+		b.reset();
+		
+		printBoard(b);
+		Move move1(Step(8,12));
+		Step step2(12,16);
+		move1.addStep(step2);
+		b.doMove(move1);
+		printBoard(b);
+		
+		assert(b.get(8) == Empty);
+		assert(b.get(12) == Empty);
+		assert(b.get(16) == White);
+	}
+	return 0;
+/*
+	====== Game zone =======
+*/
 	std::string in;
 	
-	Board b(false);
+	Board b;
 	
 	printBoard(b);
 
